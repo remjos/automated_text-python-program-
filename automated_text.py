0218473a2861fdc8535b0417535cc44ed9a2f21a
Josh Remington <josh.d.remington@gmail.com>
2023-02-04 12:54:19 -0600

    updated calls

--- automated_text.py
+++ automated_text.py
@@ -6,7 +6,7 @@ from getweather import weather
 from textbelt_api import textbelt
 
 
-weather_api_key = weather_api_key
+weather_api_key = weather
 city = "Houston"
 url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+weather_api_key+"&units=imperial"
 
@@ -28,7 +28,7 @@ def send_good_weather_message():
     resp = requests.post('https://textbelt.com/text', {
         'phone': phone_number,
         'message': 'the weather is perfect, go take a walk',
-        'key': text_api_key,
+        'key': textbelt,
 })
     print(resp.json())
     
@@ -38,7 +38,7 @@ def send_bad_weather_message():
     resp = requests.post('https://textbelt.com/text', {
         'phone': phone_number,
         'message': 'the weather is not good, humidity is ' + str(humidity) + ' so do not go outside',
-        'key': text_api_key,
+        'key': textbelt,
     })
     print(resp.json())
 
@@ -52,7 +52,7 @@ def send_message_monday():
     resp = requests.post('https://textbelt.com/text', {
         'phone': phone_number,
         'message': 'Good evening, please vacuum the entire house and conduct the daily evening cleaning tasks',
-        'key': text_api_key,
+        'key': textbelt,
     })
     print(resp.json())
 
@@ -61,7 +61,7 @@ def send_message_tuesday():
     resp = request.post('https://textbelt.com/text', {
         'phone': phone_number,
         'message': 'Clean all the bathrooms, master, guest and spare upstairs',
-        'key': text_api_key,
+        'key': textbelt,
     })
     print(resp.json())
 
@@ -70,7 +70,7 @@ def send_message_wednesday():
     resp = request.post('https://textbelt.com/text', {
         'phone': phone_number,
         'message': 'Dust everything - fans, mantel, tvs, etc.',
-        'key': text_api_key,
+        'key': textbelt,
     })
     print(resp.json())
 
@@ -79,7 +79,7 @@ def send_message_thursday():
     resp = request.post('https://textbelt.com/text', {
         'phone': phone_number,
         'message': 'Mop all floors including bathroom areas',
-        'key': text_api_key,
+        'key': textbelt,
     })
     print(resp.json())
 
@@ -88,7 +88,7 @@ def send_message_friday():
     resp = request.post('https://textbelt.com/text', {
         'phone': phone_number,
         'message': 'Monthly chore, 1st = Wipe basboards and cabinets, 2nd = Clean stove and microwave, 3rd = Clean out fridge and freezer (garage and indoor) 4th = Clean windows inside and out',
-        'key': text_api_key,
+        'key': textbelt,
     })
     print(resp.json())
 
@@ -97,7 +97,7 @@ def send_message_saturday():
     resp = request.post('https://textbelt.com/text', {
         'phone': phone_number,
         'message': 'Conduct the laundry for the week and complete any project you have been wanting to get done',
-        'key': text_api_key,
+        'key': textbelt,
     })
     print(resp.json())
 
